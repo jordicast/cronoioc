8795763fdd15ac6ea0036842d99d1e83f86e67df
Daniel Romero Jimenez <daromero1989@gmail.com>
2023-04-05 20:00:32 +0000

    joc.js

---
+++ CronoEscape/js/joc.js
@@ -0,0 +1,41 @@
+//import all modules needed for a phone aut
+
+
+import { } from "https://www.gstatic.com/firebasejs/6.0.2/firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+
+if (window.localStorage.getItem('id') == null) {
+    //redirect to the login.html page if the user is not logged in
+    window.location.href = "login.html";
+}
+
+
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDp_QjlZP6cah2JBQLIXKSbfKtOIUfZ4Tw",
+    authDomain: "cronoescape-ioc.firebaseapp.com",
+    databaseURL: "https://cronoescape-ioc-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cronoescape-ioc",
+    storageBucket: "cronoescape-ioc.appspot.com",
+    messagingSenderId: "240259839325",
+    appId: "1:240259839325:web:20ef18366fd73d2aa2a4b4",
+    measurementId: "G-QQD81WK4CE"
+};
+
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase(app);
+
+
+console.log("joc.js")
