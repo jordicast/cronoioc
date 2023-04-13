962c4b15a936d53d1cc76cea54664116a3743999
gemmma89 <127977982+gemmma89@users.noreply.github.com>
2023-04-13 10:59:14 +0200

    Update joc1.js

--- CronoEscape/js/joc1.js
+++ CronoEscape/js/joc1.js
@@ -1,17 +1,27 @@
-//pre carga de imatges
-var fondo_presente_off = new Image();
-// Establece la fuente de la imagen que quieres pre-cargar
-img.src = '../img/juego1/fondo_presente_off.jpg';
-
-
-
 function cambiarImatge() {
   var imagen = document.querySelector(".maquina");
   var so = document.getElementById("so");
-  if (imagen.getAttribute("src") == "../img/juego1/on/maquina_on.png") {
-    imagen.setAttribute("src", "../img/juego1/on/maquina_off.png");
+  if (
+    bgImgContainer.style.backgroundImage ===
+    'url("../img/juego1/fondo_presente.jpg")'
+  ) {
+    //Llum encesa
+    if (imagen.getAttribute("src") == "../img/juego1/on/maquina_on.png") {
+      imagen.setAttribute("src", "../img/juego1/on/maquina_off.png");
+      imagen.setAttribute("id", "maquinaOFF");
+    } else {
+      imagen.setAttribute("src", "../img/juego1/on/maquina_on.png");
+      imagen.setAttribute("id", "maquinaON");
+    }
   } else {
-    imagen.setAttribute("src", "../img/juego1/on/maquina_on.png");
+    //Llum apagada
+    if (imagen.getAttribute("src") == "../img/juego1/off/maquina_on.png") {
+      imagen.setAttribute("src", "../img/juego1/off/maquina_off.png");
+      imagen.setAttribute("id", "maquinaOFF");
+    } else {
+      imagen.setAttribute("src", "../img/juego1/off/maquina_on.png");
+      imagen.setAttribute("id", "maquinaON");
+    }
   }
   so.play();
 }
@@ -22,55 +32,43 @@ function apagarLlum() {
   var pc = document.querySelector(".pc");
   var photo = document.querySelector(".photo");
   var interruptor = document.querySelector(".interruptor");
-  var bgImgContainer = document.getElementById("bgImgContainer"); 
+  var bgImgContainer = document.getElementById("bgImgContainer");
 
   var so2 = document.getElementById("so-interruptor");
 
-  // Canvia la ruta de "maquina"
-  if ((maquina.getAttribute("src") == "../img/juego1/on/maquina_on.png") || (maquina.getAttribute("src") == "../img/juego1/on/maquina_off.png")) {
-    maquina.setAttribute("src", "../img/juego1/off/maquina_on.png");
-  } else {
-    maquina.setAttribute("src", "../img/juego1/on/maquina_on.png");
-  }
+  if (
+    bgImgContainer.style.backgroundImage ===
+    'url("../img/juego1/fondo_presente_off.jpg")'
+  ) {
+    //S'engega llum
+    if (maquina.id == "maquinaON") {
+      maquina.setAttribute("src", "../img/juego1/on/maquina_on.png");
+    } else {
+      maquina.setAttribute("src", "../img/juego1/on/maquina_off.png");
+    }
 
-
-  // Canvia la ruta de "tablet"
-  if (tablet.getAttribute("src") == "../img/juego1/on/tablet.png") {
-    tablet.setAttribute("src", "../img/juego1/off/tablet.png");
-  } else {
     tablet.setAttribute("src", "../img/juego1/on/tablet.png");
-  }
-
-  // Canvia la ruta de  "pc"
-  if (pc.getAttribute("src") == "../img/juego1/on/pc.png") {
-    pc.setAttribute("src", "../img/juego1/off/pc.png");
-  } else {
     pc.setAttribute("src", "../img/juego1/on/pc.png");
-  }
-
-  // Canvia la ruta de  "photo"
-  if (photo.getAttribute("src") == "../img/juego1/on/polaroid.png") {
-    photo.setAttribute("src", "../img/juego1/off/polaroid.png");
-  } else {
     photo.setAttribute("src", "../img/juego1/on/polaroid.png");
-  }
-
-  // Canvia la ruta de  "interruptor"
-  if (interruptor.getAttribute("src") == "../img/juego1/on/interruptor.png") {
-    interruptor.setAttribute("src", "../img/juego1/off/interruptor.png");
-  } else {
     interruptor.setAttribute("src", "../img/juego1/on/interruptor.png");
-  }
-
-  // Canvia la ruta de  "background"
-  if (bgImgContainer.style.backgroundImage === 'url("../img/juego1/fondo_presente.jpg")') {
-    bgImgContainer.style.backgroundImage = 'url("../img/juego1/fondo_presente_off.jpg")';
+    bgImgContainer.style.backgroundImage =
+      'url("../img/juego1/fondo_presente.jpg")';
   } else {
-    bgImgContainer.style.backgroundImage = 'url("../img/juego1/fondo_presente.jpg")';
+    //S'apaga llum
+    if (maquina.id == "maquinaON") {
+      maquina.setAttribute("src", "../img/juego1/off/maquina_on.png");
+    } else {
+      maquina.setAttribute("src", "../img/juego1/off/maquina_off.png");
+    }
+    tablet.setAttribute("src", "../img/juego1/off/tablet.png");
+    pc.setAttribute("src", "../img/juego1/off/pc.png");
+    photo.setAttribute("src", "../img/juego1/off/polaroid.png");
+    interruptor.setAttribute("src", "../img/juego1/off/interruptor.png");
+    bgImgContainer.style.backgroundImage =
+      'url("../img/juego1/fondo_presente_off.jpg")';
   }
-
-
   so2.play();
 }
+
 
 
